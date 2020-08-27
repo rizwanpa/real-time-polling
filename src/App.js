60d08f8312e2fbc94c632621abd62214981aec68
Abhishek Sharma <abhish3kk@gmail.com>
2020-08-27 23:49:02 +0530

    add mainapp.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import MainApp from './components/MainApp'
 
 function App() {
   return (
     <div className="App">
-      Live Polling
+      <MainApp></MainApp>
     </div>
   );
 }
---
+++ src/components/MainApp.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class MainApp extends Component {
+
+  render() {
+    return (
+      <div className="MainApp">
+        MainApp
+      </div>
+    )
+  }
+
+}
+
+export default MainApp
