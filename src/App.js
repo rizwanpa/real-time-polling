0f5428a2904d78427f35cc821b58ec3662fda62e
rizwanpa11 <rizz.rana@gmail.com>
2020-08-28 11:42:08 +0530

    Connected router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import { ConnectedRouter} from 'connected-react-router';
 import store, { history } from "./store";
 import "./App.css";
 import MainApp from "./components/MainApp";
@@ -7,9 +9,14 @@ import MainApp from "./components/MainApp";
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <MainApp></MainApp>
-      </div>
+    <ConnectedRouter history={history}>
+    <Switch>
+      <Route exact path='/' component={MainApp} />
+      <Route exact path='/home' component={MainApp} />
+      <Route exact path='/create-poll' component={MainApp} />
+      <Route exact path='/list-poll' component={MainApp} />
+    </Switch>
+    </ConnectedRouter>
     </Provider>
   );
 }
--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -1,73 +1,80 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { getPoll } from './../actions/index';
-import { Layout, Menu } from 'antd';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getPoll } from "./../actions/index";
+import { Layout, Menu, Anchor } from "antd";
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
   HomeOutlined,
   AreaChartOutlined,
-  PieChartOutlined,
-} from '@ant-design/icons';
+  PieChartOutlined
+} from "@ant-design/icons";
 
 const { Header, Sider, Content } = Layout;
+const { Link } = Anchor;
 
 class MainApp extends Component {
   state = {
-    collapsed: false,
+    collapsed: false
   };
 
   toggle = () => {
     this.setState({
-      collapsed: !this.state.collapsed,
+      collapsed: !this.state.collapsed
     });
   };
 
   render() {
-    console.log('main render-->',this.props);
+    console.log("main render-->", this.props);
     return (
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1" icon={<HomeOutlined />}>
-              Home
-            </Menu.Item>
-            <Menu.Item key="2" icon={<AreaChartOutlined />}>
-              Create Poll
-            </Menu.Item>
-            <Menu.Item key="3" icon={<PieChartOutlined />}>
-              List of Polls
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }}>
-            {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: this.toggle,
-            })}
-          </Header>
-          <Content
-            className="site-layout-background"
-            style={{
-              margin: '24px 16px',
-              padding: 24,
-              minHeight: 280,
-            }}
-          >
-            
-          </Content>
+      <div className="App">
+        <Layout>
+          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+              <Menu.Item key="1" icon={<HomeOutlined />}>
+                Home
+              </Menu.Item>
+              <Menu.Item key="2" icon={<AreaChartOutlined />}>
+                Create Poll
+              </Menu.Item>
+              <Menu.Item key="3" icon={<PieChartOutlined />}>
+                List of Polls
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout className="site-layout">
+            <Header className="site-layout-background" style={{ padding: 0 }}>
+              {React.createElement(
+                this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                {
+                  className: "trigger",
+                  onClick: this.toggle
+                }
+              )}
+            </Header>
+            <Content
+              className="site-layout-background"
+              style={{
+                margin: "24px 16px",
+                padding: 24,
+                minHeight: 280
+              }}
+            ></Content>
+          </Layout>
         </Layout>
-      </Layout>
+      </div>
     );
   }
 }
-const mapStateToProps = (state) =>{
-  console.log('MainApp-->',state);
-  return{...state}
-}
+const mapStateToProps = state => {
+  console.log("MainApp-->", state);
+  return { ...state };
+};
 const mapDispatchToProps = {
-  getPoll : getPoll
-}
-export default connect(mapStateToProps, mapDispatchToProps)(MainApp);
+  getPoll: getPoll
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MainApp);
